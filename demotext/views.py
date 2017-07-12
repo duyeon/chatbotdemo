1f294f856f893c35474443d152f617ea53b61a8e
duyeon <duyeon@postech.ac.kr>
2017-07-12 10:21:14 +0900

    kakao chatbot demo 0.1

---
+++ demotext/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+# Create your views here.
+
+def keyboard(request):
+
+    return JsonResponse({
+        'type': 'buttons',
+        'buttons': ['1','2','3']
+    })
+
+def home(request):
+
+    return JsonResponse({
+    
+    })
